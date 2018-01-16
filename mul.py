02843e81df98cfa425784dbc012585b446f5f778
loveho9464git config --global user.name loveho9464@gmail.com <parksungho@bagseonghoui-MacBook-Pro.local>
2018-01-16 20:50:33 +0900

    Add

--- mul.py
+++ mul.py
@@ -1,3 +1,35 @@
-i = int(input("insert number : "))
-j = int(input("insert number : "))
+# 원하는 수들의 곱셈
+i = int(input("insert 1number : "))
+j = int(input("insert 2number : "))
 print (i, "*", j, "=", i*j)
+
+# UP & DOWN 게임
+import random
+rn = random.randint(1, 100)
+count = 0
+
+while True:
+	n = int(input("Insert your choice number : "))
+	count += 1
+	if rn > n :
+		print("UP!")
+	elif rn < n :
+		print("DOWN!")
+	else :
+		print(count)
+		break
+
+# 로또 번호 뽑기
+import random
+n = []
+while len(n) < 6:
+	rn = random.randint(1, 45)
+	if rn not in n :
+		n.append(rn)
+n.sort()
+print(n)
+
+
+
+
+
