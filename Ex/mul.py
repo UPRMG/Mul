59ee3270ce0f9e269c68e9c8734335953a7afd21
UPRMG <loveho9464@gmail.com>
2020-12-21 22:42:58 +0900

    ADD

--- Ex/mul.py
+++ Ex/mul.py
@@ -25,6 +25,17 @@ rn = random.sample(range(1, 46), 6)
 rn.sort()
 print(rn)
 
+# LOTTO
+import random
+a = []
+o = random.randint(1,45)
+for i in range(6):
+    while o in a:
+        o = random.randint(1,45)
+    a.append(o)
+a.sort()
+print(a)
+
 
 # FizzBuzz
 n = int(input("insert your choice number : "))
@@ -38,4 +49,19 @@ for i in range(1, n + 1):
 		print("FizzBuzz")
 	else :
 		print(i)
+
+        
+#Fibonacci Sequence
+first = {1:1,2:1}
+
+def f(x):
+    if x in first:
+        return first[x]
+    
+    output = f(x-1)+f(x-2)
+    first[x] = output
+    
+    return output
+
+f(7)
 
